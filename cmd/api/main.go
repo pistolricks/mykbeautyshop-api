965d658c0f8317f8994fe1992c63e1d30c1705ce
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-06-26 02:22:07 -0700

    connected to db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -118,7 +118,7 @@ func main() {
 
 	var cfg config
 
-	flag.IntVar(&cfg.port, "port", 4050, "API server port")
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 
 	flag.StringVar(&cfg.db.dsn, "db-dsn", "", "PostgreSQL DSN")
